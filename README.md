ebe63fa00e9e4640e666194546186fc276b146c7
kale <18kmkf@queensu.ca>
2023-07-13 21:33:46 -0400

    Initial file setup

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# kale
-Portfolio website
+# Portfolio
+This is a portfolio website built using [Next.js](https://nextjs.org/), bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+
+
---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>hi there, I'm Kale</h1>
+      <p>I’m a third year computer science major studying at Queen’s University with a focus in Artificial Intelligence. I enjoy doing both front end, and back end development while utilizing my love for design and art</p>
+    </main>
+  );
+}
