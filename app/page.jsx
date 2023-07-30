31b500e5e4ffe37b29e3f7850cd345372666e56f
kale <18kmkf@queensu.ca>
2023-07-30 16:22:34 -0400

    setup tailwind

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,9 @@
+
 export default function Home() {
+
   return (
-    <main >
-      <h1>Hi there, <br />I'm Kale</h1>
-      <p>I’m a third year computer science major studying at Queen’s University with a focus in Artificial Intelligence. I enjoy doing both front end, and back end development while utilizing my love for design and art</p>
-    </main>
+    <div>
+      <h1 className="text-3xl font-bold">Hello, Next.js!</h1>
+    </div>
   );
 }
