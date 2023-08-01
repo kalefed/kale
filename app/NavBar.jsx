50d401a033e4747d1deb9672d52f054eea4b98cd
kale <18kmkf@queensu.ca>
2023-08-01 09:53:14 -0400

    Updating styling - colours

--- app/NavBar.jsx
+++ app/NavBar.jsx
@@ -5,10 +5,11 @@ export default function NavBar() {
     <nav className="l-1/2 pl-11 sticky top-0 h-10">
       <div className="flex justify-between p-4">
         <ul className="flex space-x-10 font-mono text-x">
-          <li><Link className='text-danger uppercase hover:text-primary active:text-primary' href="#"  >home</Link></li>
-          <li><Link className="text-danger uppercase hover:text-primary active:text-primary" href="#about">about</Link></li>
-          <li><Link className="text-danger uppercase hover:text-primary active:text-primary" href="#education">education</Link></li>
-          <li><Link className="text-danger uppercase hover:text-primary active:text-primary" href="#projects">projects</Link></li>
+          <li><Link className='text-danger uppercase hover:text-white active:text-white' href="#"  >home</Link></li>
+          <li><Link className="text-danger uppercase hover:text-white active:text-white" href="#about">about</Link></li>
+          <li><Link className="text-danger uppercase hover:text-white active:text-white" href="#education">education</Link></li>
+          <li><Link className="text-danger uppercase hover:text-white active:text-white" href="#experience">experience</Link></li>
+          <li><Link className="text-danger uppercase hover:text-white active:text-white" href="#projects">projects</Link></li>
         </ul>
       </div>
     </nav>
--- app/components/About.jsx
+++ app/components/About.jsx
@@ -6,7 +6,7 @@ export default function About() {
         <div className=" flex flex-col">
             <div className="flex flex-col justify-center h-screen" >
                     <h1 className="text-6xl font-sans font-bold uppercase text-primary">I’m a computer science AI student with an insatiable love for all things creative.</h1>
-                <p className="text-2xl font-mono text-secondary">I thrive at the intersection of technology and art, exploring full stack programming, design, and various art mediums. When I'm not immersing myself in coding, you'll find me lost in the pages of a captivating book or embarking on epic Star Wars adventures (in my head). Join me as I merge my diverse passions to create innovative solutions, thought-provoking art, and captivating user experiences. Welcome to my world of boundless possibilities!</p>
+                <p className="text-lg font-mono text-secondary pt-10">I thrive at the intersection of technology and art, exploring full stack programming, design, and various art mediums.<br/><br/>When I'm not immersing myself in coding, you'll find me lost in the pages of a captivating book or embarking on epic Star Wars adventures (in my head).<br/><br/>Join me as I merge my diverse passions to create innovative solutions, thought-provoking art, and captivating user experiences. Welcome to my world of boundless possibilities!</p>
             </div>
         </div>
     )
