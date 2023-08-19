bdad3f186318967279060a674566f4d4f351a62b
kale <18kmkf@queensu.ca>
2023-08-19 14:26:21 -0400

    Add experience accordians & styling

--- app/components/Accordian.jsx
+++ app/components/Accordian.jsx
@@ -4,25 +4,27 @@ import React, { useState } from 'react';
 // ... (previous imports)
 
 const Accordion = ({ items }) => {
-    const [activeIndex, setActiveIndex] = useState(null);
-  
-    const toggleAccordion = (index) => {
-      setActiveIndex(index === activeIndex ? null : index);
-    };
-  
-    return (
-      <div className="max-w-screen-md ">
-        {items.map((item, index) => (
-          <div key={index} className=" bg-white bg-opacity-50 rounded-md overflow-hidden mb-2">
-            <button
-              className="flex justify-between items-center w-full p-4 bg-gray-100 hover:bg-gray-200"
-              onClick={() => toggleAccordion(index)}
-            >
-              <span className="text-lg font-semibold text-gray font-mono">{item.title}</span>
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const toggleAccordion = (index) => {
+    setActiveIndex(index === activeIndex ? null : index);
+  };
+
+  return (
+    <div className="max-w-screen-md ">
+      {items.map((item, index) => (
+        <div key={index} className=" bg-white bg-opacity-50 rounded-md overflow-hidden mb-2">
+          <button
+            className="flex justify-between items-center w-full p-4 bg-gray-100 hover:bg-gray-200"
+            onClick={() => toggleAccordion(index)}
+          >
+            <div className="text-lg font-semibold text-gray font-mono">{item.title}</div>
+
+            <div className='flex justify-end'>
+              <div className="text-lg font-semibold text-gray font-mono mr-3">{item.position}</div>
               <svg
-                className={`w-5 h-5 transition-transform transform ${
-                  activeIndex === index ? 'rotate-180' : ''
-                }`}
+                className={`w-5 h-5 transition-transform transform ${activeIndex === index ? 'rotate-180' : ''
+                  }`}
                 fill="none"
                 stroke="currentColor"
                 viewBox="0 0 24 24"
@@ -34,16 +36,18 @@ const Accordion = ({ items }) => {
                   strokeWidth="2"
                   d="M19 9l-7 7-7-7"
                 />
-              </svg>
-            </button>
-            {activeIndex === index && (
+              </svg></div>
+          </button>
+          {activeIndex === index && (
+            <div>
+              <div className='p-4 font-mono italic uppercase text-gray'>{item.date}</div>
               <div className="p-4 font-sans  text-gray">{item.content}</div>
-            )}
-          </div>
-        ))}
-      </div>
-    );
-  };
-  
-  export default Accordion;
-  
+            </div>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Accordion;
--- app/components/Experience.jsx
+++ app/components/Experience.jsx
@@ -1,40 +1,58 @@
 import Accordian from "./Accordian"
 const items = [
-    {
-      title: 'Junior Analyst - Statistics Canada',
-      content: 'Developed a metadata search engine with React and Elasticsearch, enhancing dataset visibility through a standard knowledge graph format and SKOS data model. Designed RESTful APIs using FASTAPI and OpenAPI, while implementing Python for encoding and improving statistical dataset discoverability, with a focus on effective use case testing and validation.',
-    },
-    {
-      title: 'Student Developer - Canada Revenue Agency',
-      content: 'Enhanced software reliability by implementing unit tests, ensuring higher code quality. Programmed dynamic conditional rendering in a Java Swing application, resulting in improved user interface flexibility and interaction.',
-    },
-    {
-        title: 'Full Stack developer - COMPSA',
-        content: 'Collaborated within a development team to build an admin panel using Next.js, Bulma CSS, and Fetch to interface with APIs. Contributed to various web projects, including the COMPSA website and merch store, demonstrating proficiency in Node.js, Python, React/Next.js, HTML, CSS, and GitHub.',
-      },
-      {
-        title: 'Stduent Subcommittee Coordinator - Statistics Canada',
-        content: 'Content of Section 2',
-      },
-      {
-        title: 'Student Pal Mentorship Program - Mentor',
-        content: 'Content of Section 2',
-      },
-      {
-        title: 'Orientation Leader - Queens Univeristy',
-        content: 'Content of Section 2',
-      },
-  ];
+  {
+    title: 'Student Developer',
+    position: 'Canada Revenue Agency',
+    content: 'Enhanced software reliability by implementing unit tests, ensuring higher code quality. Programmed dynamic conditional rendering in a Java Swing application, resulting in improved user interface flexibility and interaction.',
+    date: 'May 2023 - Present'
+
+  },
+  
+  {
+    title: 'Full Stack developer',
+    position: 'COMPSA',
+    content: 'Collaborated within a development team to build an admin panel using Next.js, Bulma CSS, and Fetch to interface with APIs. Contributed to various web projects, including the COMPSA website and merch store, demonstrating proficiency in Node.js, Python, React/Next.js, HTML, CSS, and GitHub.',
+    date: 'June 2023 - Present'
+
+  },
+  {
+    title: 'Junior Analyst',
+    position: 'Statistics Canada',
+    content: 'Developed a metadata search engine with React and Elasticsearch, enhancing dataset visibility through a standard knowledge graph format and SKOS data model. Designed RESTful APIs using FASTAPI and OpenAPI, while implementing Python for encoding and improving statistical dataset discoverability, with a focus on effective use case testing and validation.',
+    date: 'May 2021 - April 2023'
+  },
+  {
+    title: 'Orientation Leader',
+    position: 'Queens Univeristy',
+    content: 'Guided and facilitated the transition of incoming first-year university students as an orientation leader, providing essential information, support, and a welcoming atmosphere during the orientation program.',
+    date: 'Starting September 2023'
+
+  },
+  {
+    title: 'Student Subcommittee Coordinator',
+    position: 'Statistics Canada',
+    content: 'Took charge of coordinating and directing weekly SubCommittee meetings, overseeing task progress, fostering engagement, and delivering reports on accomplishments and schedules. Pioneered initiatives to enhance student engagement and build a remote work community, including event ideation, organization, and hosting.',
+    date: 'May 2022 - September 2022'
+
+  },
+  {
+    title: 'Student Pal Mentorship Program Mentor',
+    position: 'Statistics Canada',
+    content: 'Served as a guiding pal in the Student Pals Program at Statistics Canada, mentoring new student employees through onboarding and work-related challenges, including bi-weekly meetings to address various aspects of their experience.',
+    date: 'October 2021 - August 2022'
+
+  },
+];
 
 export default function Experience() {
-    return (
-        <div className="flex flex-col h-screen">
-            <h1 className="text-6xl font-sans font-bold uppercase text-white pt-44">Professional And Volunteer Experience</h1>
-            <div className="flex justify-center pt-14 flex-grow">
-                <div className="w-1/2">
-                    <Accordian items={items}/>
-                </div>
-            </div>
+  return (
+    <div className="flex flex-col h-screen">
+      <h1 className="text-6xl font-sans font-bold uppercase text-white pt-44">Professional And Volunteer Experience</h1>
+      <div className="flex justify-center pt-14 flex-grow">
+        <div className="w-1/2">
+          <Accordian items={items} />
         </div>
-    )
+      </div>
+    </div>
+  )
 }
